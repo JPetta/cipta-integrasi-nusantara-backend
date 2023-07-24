697b0466feb40030849c62a1bfab93de120c330c
Jovi Petra <jovipetraratulangi@gmail.com>
2023-07-24 22:02:34 +0700

    feat: added register and login

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const errorHandler = require('./errorHandler')
+const cors = require('cors')
+const { User } = require('./models')
+const { checkPassword, hashPassword } = require('./helpers/bcrypt')
+const { signToken, decodeToken } = require('./helpers/jwt')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+
+app.post("/register", async (req, res, next) => {
+    try {
+        const payload = req.body
+        const createdUser = await User.create(payload)
+        res.status(201).json({ message: `User with email ${payload.email} is created` })
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.post("/login", async (req, res, next) => {
+    try {
+        if (!req.body.email || !req.body.password) throw ({ name: "EmailPasswordMissing" })
+
+        const user = await User.findOne({ where: { email: req.body.email } })
+        if (!user) throw ({ name: "InvalidCredentials" })
+        req.body.id = user.dataValues.id
+
+        const isPasswordValid = checkPassword(req.body.password, user.password)
+        if (!isPasswordValid) throw ({ name: "InvalidCredentials" })
+
+        let { dataValues } = user
+        const generatedToken = signToken({
+            id: dataValues.id,
+            email: dataValues.email,
+            role: dataValues.role
+        })
+
+        res.status(200).json({ message: "Login success", token: generatedToken })
+
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
+}
+)
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`App is running on port : ${port} ~!`)
+})
